20844042526340f63d74f07f582da95abb57ff11
Axel <axel@vanabkou.de>
2025-11-01 12:46:58 +0100

    Added vars

--- natexp_compiler.v
+++ natexp_compiler.v
@@ -13,12 +13,20 @@ Consider the following expression language:
 〈exp〉 ::= 〈literal〉 | 〈exp〉 + 〈exp〉 | . . .
 〈literal〉 ::= 0 | 1 | 2 | . . .
 
-Give an Inductive definition of a datatype Exp of (the abstract syntax
+Give an Inductive definition :of a datatype Exp of (the abstract syntax
 for) 〈exp〉s.
  *)
+Require Import Stdlib.Strings.String.
+Require Import Stdlib.Lists.List.
+
 Definition lit := nat. 
+Definition env := string -> option lit.
+Definition empty : env := (fun _ => None).
+Definition update (f : env) (x : string) (l : lit) : env :=
+  (fun y => if String.eqb x y then Some l else f y).
 Inductive Exp : Set :=
   | Elit : lit -> Exp
+  | Evar : string -> Exp
   | Eplus : Exp -> Exp -> Exp
   | Emin : Exp -> Exp -> Exp
   | Emult : Exp -> Exp -> Exp.
@@ -31,17 +39,41 @@ eval: Exp -> nat
 giving a semantics for 〈exp〉s.
 
  *)
-Fixpoint eval (e:Exp) {struct e} : lit :=
+Fixpoint eval (e:Exp) (f:env) {struct e} : option lit :=
 match e with
-| Elit l      => l
-| Eplus e1 e2 => plus (eval e1) (eval e2)
-| Emin  e1 e2 => min (eval e1) (eval e2)
-| Emult e1 e2 => mult (eval e1) (eval e2)
+| Elit l      => Some l
+| Evar x      => f x
+| Eplus e1 e2 => 
+    match eval e1 f, eval e2 f with
+    | Some l1, Some l2 => Some (plus l1 l2)
+    | _,_ => None
+    end
+| Emin  e1 e2 =>
+    match eval e1 f, eval e2 f with
+    | Some l1, Some l2 => Some (min l1 l2)
+    | _,_ => None
+    end
+| Emult e1 e2 => 
+    match eval e1 f, eval e2 f with
+    | Some l1, Some l2 => Some (mult l1 l2)
+    | _,_ => None
+    end
 end.
 
 Definition exp0 := Elit 0.
-Definition exp1 := Eplus (Elit 1) (Elit 2).
-Eval compute in (eval (exp1)).
+Definition exp1 := Elit 1.
+Definition exp2 := Evar "x".
+Definition exp3 := Eplus (Evar "x") (Elit 2).
+Definition exp4 := Eplus (Elit 1) (Elit 2).
+Definition env0 := update empty "x" 4.
+Definition env1 := update env0 "y" 6.
+Definition env2 := update empty "y" 6.
+
+Eval compute in (eval exp3 empty).
+Eval compute in (eval exp3 env0).
+Eval compute in (eval exp3 env1).
+Eval compute in (eval exp3 env2).
+
 
 (*
    --------------------------------------------
@@ -50,6 +82,7 @@ for 〈exp〉s.
  *)
 Inductive RPN : Set :=
   | RPNlit : lit -> RPN
+  | RPNvar : string -> RPN
   | RPNbinop : option RPN -> option RPN -> (lit -> lit -> lit) -> RPN
 .
 
@@ -61,6 +94,7 @@ rpn : Exp -> RPN
 Fixpoint rpn (e:Exp) {struct e} : RPN :=
 match e with
 | Elit l => RPNlit l
+| Evar x => RPNvar x
 | Eplus e1 e2 => RPNbinop (Some (rpn e1)) (Some (rpn e2)) plus
 | Emin  e1 e2 => RPNbinop (Some (rpn e1)) (Some (rpn e2)) min
 | Emult e1 e2 => RPNbinop (Some (rpn e1)) (Some (rpn e2)) mult
@@ -71,11 +105,12 @@ end.
 Write an evaluator rpn_eval for RPN, returning an option nat.
  *)
 
-Fixpoint rpn_eval (r:RPN) {struct r} : option lit :=
+Fixpoint rpn_eval (r:RPN) (f:env) {struct r} : option lit :=
 match r with
 | RPNlit l => Some l
+| RPNvar x => f x
 | RPNbinop (Some r1) (Some r2) op => 
-    match (rpn_eval r1), (rpn_eval r2) with
+    match (rpn_eval r1 f), (rpn_eval r2 f) with
     | Some r1', Some r2' => Some (op r1' r2')
     | _, _ => None
     end
@@ -84,23 +119,27 @@ end.
 
 Definition rpn0 := RPNlit 0.
 Definition rpn1 := RPNbinop (Some (RPNlit 5)) (Some (RPNlit 6)).
-Definition rpn2 := RPNbinop None (Some (RPNlit 6)).
-Eval compute in (rpn_eval (rpn1 plus)).
-Eval compute in (rpn_eval (rpn1 mult)).
-Eval compute in (rpn_eval (rpn2 plus)).
+Definition rpn2 := RPNbinop (Some (RPNvar "x")) (Some (RPNlit 6)).
+Definition rpn3 := RPNbinop None (Some (RPNlit 6)).
+Eval compute in (rpn_eval (rpn1 plus) empty).
+Eval compute in (rpn_eval (rpn1 mult) empty).
+Eval compute in (rpn_eval (rpn2 plus) empty).
+Eval compute in (rpn_eval (rpn2 plus) env0 ).
+Eval compute in (rpn_eval (rpn2 plus) env1 ).
+Eval compute in (rpn_eval (rpn2 plus) env2 ).
 (*
    --------------------------------------------
 Prove that
 5forall e:Exp, Some (eval e) = rpn_eval (rpn e)
  *)
-Definition exp2 := Eplus (Elit 1) (Elit 2).
-Eval compute in (Some (eval exp2)).
-Eval compute in (rpn_eval (rpn exp2)
+Definition exp5 := Eplus (Elit 1) (Elit 2).
+Eval compute in (eval exp5).
+Eval compute in (rpn_eval (rpn exp5)
 ).
 
 
 
-Lemma Equivalence_Eval_RPNEval : forall e:Exp, Some (eval e) = rpn_eval (rpn e).
+Lemma Equivalence_Eval_RPNEval : forall e:Exp, eval e = rpn_eval (rpn e).
 Proof.
   induction e;simpl;try rewrite <- IHe1, <- IHe2; reflexivity.
 Qed.
